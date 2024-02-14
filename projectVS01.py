f0a0fbf40a155ee656f7d3cd644d12899f935b54
nadarktong <neangez@gmail.com>
2024-02-14 09:40:47 +0700

    Update projectVS01.py

--- projectVS01.py
+++ projectVS01.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.image('img/g2.jpg')
+st.image('img/ok.jpg')
 st.header("การนำเสนอสถิติการเกิดอุบัติเหตุของประเทศไทย")
 
 col1,col2=st.columns(2)
@@ -11,13 +11,30 @@ with col1:
     st.write("2,5600")
 with col2:
     st.subheader("จำนวนผู้เสียชีวิต")
-    st.write("2,5600")
+    st.write("2,5600")        
 
-dt=pd.read_excel('data/opendata-rtddi-54-66-9month.xlsx')
+dt.pd.read_excel('Data/DT01.xlsx')    
 
+st.write(dt.head(1))
+
+#st.write()
 NumM=dt[dt['Sex']=='ชาย'].count()
-Numโ=dt[dt['Sex']=='หญิง'].count()
+NumF=dt[dt['Sex']=='หญิง'].count()
 
-dtSex=[NumM,NumF]
-dtSex=pd.DataFrame(dtSex)
-st.bar_chart(dtsexb)
+dt.subheader('ชาย')
+dt.subheader(NumM[1])
+dt.subheader('หญิง')
+dt.subheader(NumF[1])
+dtSex=[NumM[1],NumF[1]]
+dtSexb=pd.DataFrame(dtSexb,index=["ชาย,"หญิง])
+st.bar_chart(dtSexd)
+
+import matplotlib.pyplot as plt
+labels = 'ชาย', 'หญิง'
+sizes = [NumM[1],NumF[1]]
+explode = (0, 0.1)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
+st.pyplot(fig1)
